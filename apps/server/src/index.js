88f3bd87ac52b7be5607aec5f6600e0fbff3d71c
Xemko <xemkos@gmail.com>
2023-12-16 02:46:00 +0200

    feat:m:major - intergration of mongodg, mongoose and dotenv

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -1,7 +1,41 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const mainRoutes = require('./router/v1/main.routes');
-const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect(process.env.DB_URI);
+const PORT = process.env.PORT;
+const myDb = mongoose.connection;
+
+myDb.on('error', console.error.bind(console, "connection error:"));
+myDb.once("open", () => {
+
+    // Check connection status
+    console.log('Mongoose connection status:', mongoose.connection.readyState ? 'Connected to ' + myDb.name : 'Disconnected');
+   
+})
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String
+  });
+  
+  // Define the User model
+  const User = mongoose.model('User', UserSchema);
+  
+// Define an async function to query the users collection
+const getUsers = async () => {
+    try {
+      const users = await User.find();
+      console.log('Users:', users);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+getUsers();  
 
 app.use('/api', mainRoutes);
 
