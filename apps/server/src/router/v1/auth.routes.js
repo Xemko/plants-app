d9f94ede7a2649da9a4831609376075e9018314b
Xemko <xemkos@gmail.com>
2023-12-21 22:30:36 +0200

    logic refactoring

--- apps/server/src/router/v1/auth.routes.js
+++ apps/server/src/router/v1/auth.routes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {auth, signIn, registerUser, protected} = require('../../controllers/auth.controller')
 
-router.post('/sign-in', signIn);
-router.post('/register', registerUser);
-router.post('validate', auth);
-router.get('/protected', auth, protected)
+router.post('sign-in', signIn);
+router.post('register', registerUser);
+router.get('validate', auth, protected)
 
 
 module.exports = router;
