1c98c215034119fa418c43bc25fac0bfea6e2956
tgcaspa <cmplerec@gmail.com>
2023-12-27 22:34:11 +0200

    library infra

---
+++ apps/client/projects/infra/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of infra
+ */
+
+export * from './lib/directives/first-error.pipe';
+export * from './lib/models/environment.model';
+export * from './lib/models/server-response.model';
